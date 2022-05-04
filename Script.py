73354383c0d084110048ce77ef7c99865c785147
sherlocksha1 <98747957+sherlocksha1@users.noreply.github.com>
2022-05-04 16:02:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,18 +8,18 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/movies_club_2018>𝙼𝙾𝚅𝙸𝙴𝚂 𝙲𝙻𝚄𝙱</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/peter_parker_10>𝗣𝗘𝗧𝗘𝗥 𝗣𝗔𝗥𝗞𝗘𝗥</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
+- The Source is not open
+- Source Creditor - <a href=https://t.me/peter_parker_10>𝗣𝗘𝗧𝗘𝗥 𝗣𝗔𝗥𝗞𝗘𝗥</a>   
 
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
+<b>Source ആവിശ്യം ഉള്ളവർ Contact ചെയ്യുക</b>
+- <<a href=https://t.me/peter_parker_10>𝗖𝗼𝗻𝘁𝗮𝗰𝘁📞</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
